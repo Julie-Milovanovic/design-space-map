30865ae5f199c1dd3e8fdd9b76f29635fa2b947b
Julie-Milovanovic <94129152+Julie-Milovanovic@users.noreply.github.com>
2021-11-11 19:08:55 -0500

    Update design_space.py

--- design_space.py
+++ design_space.py
@@ -314,8 +314,7 @@ session_01 = pd.read_csv("https://raw.githubusercontent.com/Julie-Milovanovic/de
 # Organization page
 st.title("Representation of the design space for one design session of a team")
 st.subheader("About the graph")
-st.markdown('This graph represents design concepts formulated by designer during a design session.\ 
-		The deisgn re')
+st.markdown('This graph represents design concepts formulated by designer during a design session. The design re')
 st.markdown("Select process on the side panel to characterize the design space.")
 
 
