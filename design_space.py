bdb8e46782cef72977c42e2d43165632ad543e79
Julie-Milovanovic <94129152+Julie-Milovanovic@users.noreply.github.com>
2022-07-02 14:39:43 +0100

    Update design_space.py

--- design_space.py
+++ design_space.py
@@ -1,3 +1,6 @@
+# This material is based upon work supported by the National Science Foundation under Grant No. CMMI-1762415. 
+#Any opinions, findings, and conclusions or recommendations expressed in this material are those of the authors 
+#and do not necessarily reflect the views of the National Science Foundation.
 
 # core packages
 import streamlit as st 
